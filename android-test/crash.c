8fc3f143e8dffad206f91b9988be1d0b851b857b
hyrathon <hyrathon@gmail.com>
2018-10-16 19:12:14 +0800

    manual merge changes from https://github.com/kkuehl/android-afl, issues still exists

--- android-test/crash.c
+++ android-test/crash.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,16 +8,16 @@ int main()
 {
   char buf[5];
 
-  memset(buf, 5, 0);
+  memset(buf, 0, 5);
 
   read(0, buf, 5);
 
   if (buf[0] == 'c')
-  	if (buf[1] == 'r')
-  		if (buf[2] == 'a')
-  			if (buf[3] == 's')
-  				if (buf[4] == 'h')
-  					abort();
+    if (buf[1] == 'r')
+      if (buf[2] == 'a')
+        if (buf[3] == 's')
+          if (buf[4] == 'h')
+            abort();
 
   return 0;
 }
