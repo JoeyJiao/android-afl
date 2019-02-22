10121a16c4541fea69b28e2f6959200a4f5f77eb
Joey Jiao <joeyjiaojg@163.com>
2019-02-22 14:21:48 +0800

    Fix aosp x86_64 emulator afl-fuzz not work due to afl-llvm-rt

--- android-test/Android.mk
+++ android-test/Android.mk
@@ -1,23 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-#export TARGET_TOOLCHAIN_PREFIX := ./prebuilts/clang/host/linux-x86/clang-4691093/bin/
-#export HOST_TOOLCHAIN_PREFIX := ./prebuilts/clang/host/linux-x86/clang-4691093/bin/
-
 include $(CLEAR_VARS)
 
-#LOCAL_SRC_FILES := crash.c
-#LOCAL_MODULE := crash
-#include $(BUILD_EXECUTABLE)
-#include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := crash.c
+LOCAL_CFLAGS := -static -g -O0
 LOCAL_MODULE := afl-crash
 
-# test afl-gcc/afl-g++ on ARM/Android
-# to build: TEST_GCC_ARM=true mm
 ifeq ($(TEST_GCC_ARM), true)
 export AFL_CC := $(TARGET_CC)
 # expoet AFL_CXX := $(TARGET_CXX)
+#export TARGET_TOOLCHAIN_PREFIX := ./prebuilts/clang/host/linux-x86/clang-4691093/bin/
 export AFL_AS := $(TARGET_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := false
 LOCAL_CC := afl-gcc
@@ -25,12 +17,11 @@ LOCAL_CC := afl-gcc
 include $(BUILD_EXECUTABLE)
 endif
 
-# test afl-gcc/afl-g++ on host
-# to build: TEST_GCC_HOST=true mm
 ifeq ($(TEST_GCC_HOST), true)
 export AFL_CC=$(HOST_CC)
 # export AFL_CXX=$(HOST_CXX)
 LOCAL_MULTILIB := 32
+#export HOST_TOOLCHAIN_PREFIX := ./prebuilts/clang/host/linux-x86/clang-4691093/bin/
 export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := true
 LOCAL_CC := afl-gcc
@@ -38,23 +29,16 @@ LOCAL_CC := afl-gcc
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
-# test afl-clang/afl-clang++ on ARM/Android
-# to build: TEST_CLANG_ARM=true mm
 ifeq ($(TEST_CLANG_ARM), true)
-export AFL_CC=$(CLANG)
-#export AFL_CXX=$(CLANG_CXX)
 export AFL_AS=prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/x86_64-linux-android/bin/as
 #export AFL_AS=prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/aarch64-linux-android/bin/as
 LOCAL_CLANG := true
-LOCAL_CC := afl-clang
+LOCAL_CC := AFL_CC=$(CLANG) afl-clang
+LOCAL_CXX := AFL_CXX=$(CLANG_CXX) afl-clang++
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-#LOCAL_CFLAGS := -static -g -v
-# LOCAL_CC := afl-clang++
 include $(BUILD_EXECUTABLE)
 endif
 
-# test afl-clang/afl-clang++ on host
-# to build: TEST_CLANG_HOST=true mm
 ifeq ($(TEST_CLANG_HOST), true)
 export AFL_CC=$(CLANG)
 # export AFL_CXX=$(CLANG_CXX)
@@ -66,50 +50,30 @@ LOCAL_CC := afl-clang
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
-# test afl-clang-fast/afl-clang-fast++ on ARM/Android
-# to build: TEST_CLANG_FAST_ARM=true mm -B
 ifeq ($(TEST_CLANG_FAST_ARM), true)
-# LOCAL_CXX := afl-clang-fast++
-#LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt.o
+LOCAL_MULTILIB := 32
+LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-32.o
 include android-afl/android-test/afl.mk
 include $(BUILD_EXECUTABLE)
 endif
 
-# test afl-clang-fast/afl-clang-fast++ on aarch64/Android
-# to build: TEST_CLANG_FAST_AARCH64=true mm -B
 ifeq ($(TEST_CLANG_FAST_AARCH64), true)
-export AFL_CC=/usr/bin/clang
-# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_MULTILIB := 64
-LOCAL_CLANG := true
-LOCAL_CC := afl-clang-fast
-# LOCAL_CXX := afl-clang-fast++
-LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-64.o
+#LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-64.o
+include android-afl/android-test/afl.mk
 include $(BUILD_EXECUTABLE)
 endif
 
-# test afl-clang-fast/afl-clang-fast++ on 64bit host
-# to build: TEST_CLANG_FAST_HOST=true mm -B
 ifeq ($(TEST_CLANG_FAST_HOST), true)
-export AFL_CC=/usr/bin/clang
-# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_MULTILIB := 64
-LOCAL_CLANG := true
-LOCAL_CC := afl-clang-fast
-# LOCAL_CXX := afl-clang-fast++
 LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-64.o
+include android-afl/android-test/afl.mk
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
-# test afl-clang-fast/afl-clang-fast++ on 32bit host
-# to build: TEST_CLANG_FAST_HOST_32=true mm -B
 ifeq ($(TEST_CLANG_FAST_HOST_32), true)
-export AFL_CC=/usr/bin/clang
-# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_MULTILIB := 32
-LOCAL_CLANG := true
-LOCAL_CC := afl-clang-fast
-# LOCAL_CXX := afl-clang-fast++
 LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-32.o
+include android-afl/android-test/afl.mk
 include $(BUILD_HOST_EXECUTABLE)
 endif
