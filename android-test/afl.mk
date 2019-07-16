c6bd3c8397061c2b9bfeffe2b51f66931487b4eb
Joey Jiao <joeyjiaojg@163.com>
2019-07-16 15:36:40 +0800

    Need to export AFL_PATH and AFL_CC | AFL_CXX

--- android-test/afl.mk
+++ android-test/afl.mk
@@ -1,3 +1,3 @@
 LOCAL_CLANG := true
-LOCAL_CC := AFL_PATH=$(HOST_OUT)/afl AFL_CC=$(CLANG) $(HOST_OUT)/bin/afl-clang-fast
-LOCAL_CXX := AFL_PATH=$(HOST_OUT)/afl AFL_CXX=$(CLANG_CXX) $(HOST_OUT)/bin/afl-clang-fast++
+LOCAL_CC := $(HOST_OUT)/bin/afl-clang-fast
+LOCAL_CXX := $(HOST_OUT)/bin/afl-clang-fast++
